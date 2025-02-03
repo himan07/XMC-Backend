fec0fc446f457221e85a8e5ec19a5af50881cab1
Himanshu yadav <himan9714@gmail.com>
2025-02-03 21:25:45 +0530

    Update uploadcert.controller.js

--- src/controllers/uploadcert.controller.js
+++ src/controllers/uploadcert.controller.js
@@ -92,7 +92,7 @@ exports.uploadCertificate = (req, res) => {
       return res.status(500).json({
         status: "failed,
         message: "Internal Server Error",
-        error: error.message,
+        error: error,
       });
     }
   });
